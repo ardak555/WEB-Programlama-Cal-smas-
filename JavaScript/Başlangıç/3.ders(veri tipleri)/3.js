72a0f9b516dea4f1929c35c3021b39b452f78e4d
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-07-31 14:43:26 +0300

    Add files via upload

---
+++ JavaScript/Başlangıç/3.ders(veri tipleri)/3.js
@@ -0,0 +1,22 @@
+// var a=20;
+// var b=10;
+// var c=80;
+// console.log(a,b,c);
+
+//Primitive Veri tipleri
+var a=10;
+console.log(typeof a);
+
+var name="Arda";
+console.log(typeof name)
+
+var b=true;
+console.log(typeof b)
+
+var h=null;
+console.log( h );
+console.log(typeof h);
+
+var l;
+console.log(l);
+console.log(typeof l);
