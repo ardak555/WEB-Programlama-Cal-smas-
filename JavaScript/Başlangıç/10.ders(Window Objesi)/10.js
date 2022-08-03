afb90a9ac643953c1c057f90cd8b70c35d5157c1
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-08-03 10:49:25 +0300

    Add files via upload

---
+++ JavaScript/Başlangıç/10.ders(Window Objesi)/10.js
@@ -0,0 +1,31 @@
+console.log(window);
+
+//confirm eminmisin sorularını sorularını
+//prompt kullanıcıdan inupt alıyoruz
+
+//Alert
+
+/*alert("merhaba");
+
+const cevap = confirm("Eminmisiniz?");
+if(cevap){
+    console.log("a")
+}
+else{
+    console.log("b")
+}
+*/
+// const cvp = prompt("Güncellenecek ifadeyi girin...");
+
+let value;
+
+ value = window;
+ value = window.location;
+ value = window.location.host;
+ value = window.location.hostname;
+ value = window.location.port;
+ value = window.location.href;
+
+ value= window.outerHeight;
+ value = window.outerWidth;
+ console.log(value)
